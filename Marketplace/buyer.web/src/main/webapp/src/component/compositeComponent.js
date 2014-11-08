0583f237135a1d9aa2c86673a54a571916bb1939
unknown <miso4204.201420@gmail.com>
2014-11-08 00:03:23 -0500

    Commit automatico_08/11/2014 12:03 a.m.

--- Marketplace/buyer.web/src/main/webapp/src/component/compositeComponent.js
+++ Marketplace/buyer.web/src/main/webapp/src/component/compositeComponent.js
@@ -57,7 +57,25 @@ define(['controller/selectionController', 'model/cacheModel', 'component/_CRUDCo
             alert('Facebook');
         },
         google: function() {
-            alert('Google');
+            hello.init({
+                'google' : '491584393580-kslq1aoen62du8rhgmlepdhudg12ldbb.apps.googleusercontent.com'
+            },
+            {
+                scope : 'email',
+                oauth_proxy: 'https://auth-server.herokuapp.com/proxy'
+            });
+            hello('google').login().then(function(){
+                hello('google').api('/me').then(function(response){
+                    $("#username").val(response.email);
+                    $("#email").val(response.email);
+                    $("#name").val(response.name);
+                    $("#firstName").val(response.first_name);
+                    $("#lastName").val(response.last_name);
+                    $("#gender").val(response.gender);
+                });
+            }, function(e){
+                alert("Error al Autenticar: " + e.error.message);
+            });
         }
     });
 
