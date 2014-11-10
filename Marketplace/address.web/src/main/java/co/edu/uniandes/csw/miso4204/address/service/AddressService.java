8824851317d76e81a9363c30835efd32a33721a5
unknown <miso4204.201420@gmail.com>
2014-11-10 16:23:27 -0500

    Commit automatico_10/11/2014 04:23 p.m.

--- Marketplace/address.web/src/main/java/co/edu/uniandes/csw/miso4204/address/service/AddressService.java
+++ Marketplace/address.web/src/main/java/co/edu/uniandes/csw/miso4204/address/service/AddressService.java
@@ -38,7 +38,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/addresss")
+@Path("/addresses")
 @Stateless
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
