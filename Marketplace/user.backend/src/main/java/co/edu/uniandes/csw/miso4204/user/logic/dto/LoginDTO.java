328199a5895067b5db912f212b87fce299e44f8f
miso4204-201420 <miso4204.201420@gmail.com>
2014-11-13 17:29:23 -0500

    Revert "Revert "Se realiza la configuracion de bonus, reward y user. Se adiciona..."

---
+++ Marketplace/user.backend/src/main/java/co/edu/uniandes/csw/miso4204/user/logic/dto/LoginDTO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniandes.csw.miso4204.user.logic.dto;
+
+/**
+ *
+ * @author Jj.alarcon10
+ */
+public class LoginDTO {
+	
+	public String userName;
+	public String password;
+	public boolean rememberMe;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isRememberMe() {
+		return rememberMe;
+	}
+
+	public void setRememberMe(boolean rememberMe) {
+		this.rememberMe = rememberMe;
+	}
+	
+	
+	
+}
