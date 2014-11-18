8801cf2ea01a388d2af53b8b7a2150c75a5fea3d
webridertrilogy <webridertrilogy@hotmail.com>
2014-11-18 13:19:13 -0500

    'DIAN 20141118 - Creacion de proyectos de wishlist con los cambios realizados

---
+++ Marketplace/wishlist.web/src/main/webapp/src/component/wishListMasterComponent.js
@@ -0,0 +1,25 @@
+define(['component/_wishListMasterComponent'],function(_WishListMasterComponent) {
+    App.Component.WishListMasterComponent = _WishListMasterComponent.extend({
+		postInit: function(){
+			//Escribir en este servicio las instrucciones que desea ejecutar al inicializar el componente
+		},
+                addItems: function (params) {
+                     var list = this.whishListItemComponent.getRecords();
+                     for (var idx in params) {
+                         var productId = params[idx].productId;
+                         //this.wishListItemComponent.addRecords({productId: productId, quantity: 1});
+
+                         var model = _.findWhere(list, {productId: productId});
+                         if (model) {
+                             //model.quantity++;
+                             //this.shoppingCartItemComponent.updateRecord(model);
+                         } else {
+                             this.whishListItemComponent.addRecords({productId: productId});    
+                         }
+                     }
+                 }
+ 
+    });
+
+    return App.Component.WishListMasterComponent;
+});
