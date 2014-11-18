b4a74452e06cb019df833e5dbc2005c9bbf0243a
webridertrilogy <webridertrilogy@hotmail.com>
2014-11-18 13:17:25 -0500

    'DIAN 20141018 - Modificacion de solo wishlist

--- Marketplace/wishlist.web/src/main/webapp/src/component/wishListMasterComponent.js
+++
@@ -1,9 +0,0 @@
-define(['component/_wishListMasterComponent'],function(_WishListMasterComponent) {
-    App.Component.WishListMasterComponent = _WishListMasterComponent.extend({
-		postInit: function(){
-			//Escribir en este servicio las instrucciones que desea ejecutar al inicializar el componente
-		}
-    });
-
-    return App.Component.WishListMasterComponent;
-});
