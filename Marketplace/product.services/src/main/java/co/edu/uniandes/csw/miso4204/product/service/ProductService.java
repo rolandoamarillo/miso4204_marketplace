568e3c577551f0932e4fee8b9300a51d172e6dea
cristo <ca.rodriguez18@uniandes.edu.co>
2014-11-22 10:27:23 -0500

    Buscar picture

--- Marketplace/product.services/src/main/java/co/edu/uniandes/csw/miso4204/product/service/ProductService.java
+++ Marketplace/product.services/src/main/java/co/edu/uniandes/csw/miso4204/product/service/ProductService.java
@@ -66,4 +66,5 @@ public class ProductService extends _ProductService {
                                                maxRecords, 
                                                firstRecord);
     }
+    
 }
