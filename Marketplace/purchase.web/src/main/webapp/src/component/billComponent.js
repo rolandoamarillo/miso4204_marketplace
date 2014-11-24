ebf59ca938573d7aa02d14498c81d81c1d4c0835
Cindy Hernández <cvho31@gmail.com>
2014-11-23 21:56:51 -0500

    Revert "Cambios Front End"
    This reverts commit 96899fc7b8e2075bc2ef627dd35977d5462a5a67.

--- Marketplace/purchase.web/src/main/webapp/src/component/billComponent.js
+++ Marketplace/purchase.web/src/main/webapp/src/component/billComponent.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-define(['component/listComponent', 'component/toolbarComponent' ], function(listCp, toolbarCP) {
+define(['component/shoppingCartComponent', 'component/toolbarComponent' ], function(shoppingCartCp, toolbarCP) {
     App.Component.Bill = App.Component.BasicComponent.extend({
         initialize: function(options) {
             this.componentId = App.Utils.randomInteger();
@@ -13,19 +13,22 @@ define(['component/listComponent', 'component/toolbarComponent' ], function(list
                 this.addressList = options.addressList;
             }
             
-            if(options.creditCardList){
-                this.creditCardList = options.creditCardList;
+            if(options.paymentList){
+                this.paymentList = options.paymentList;
             }
             if(options.purchaseIntegrator){
                 this.purchaseIntegrator = options.purchaseIntegrator;
             }
             
-            var date = new Date();
-            $('#purchaseDate').val(date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear());
-            
             this.toolbar();
-            this.productList();
+            this.setupShoppingCart();
             this.loadData();
+        },
+        
+        setupShoppingCart: function (){
+            this.shoppingCartComponent = new shoppingCartCp();
+            this.shoppingCartComponent.initialize();
+            this.shoppingCartComponent.toolbarComponent.display(false);
         },
         
         loadData: function(){
@@ -34,16 +37,25 @@ define(['component/listComponent', 'component/toolbarComponent' ], function(list
             $('#sendCity').val(this.addressList.attributes.city);
             $('#sendCountry').val(this.addressList.attributes.country);
             
-            $('#payMode').val(this.creditCardList.attributes.accountType);
-            $('#payNumber').val(this.creditCardList.attributes.cardNumber);
-            $('#branch').val(this.creditCardList.attributes.branch);
-            $('#payPoints').val(this.creditCardList.attributes);
+            $('#payMode').val(this.paymentList.attributes.name);
+//            $('#payNumber').val(this.paymentList.attributes);
+//            $('#payPoints').val(this.paymentList.attributes);
+        },
+                
+        render: function(parent){
+            this.shoppingCartComponent.render('list');
+            $('#'+parent).append($('#bill').show());
         },
         
         toolbar: function(){
+            console.log("entro3");
             this.toolbarComponent = new toolbarCP({componentId: "toolbar", name: "Confirm and Pay"});
+            console.log("entro1");
             this.toolbarComponent.initialize({componentId: "toolbar", name: "Confirm and Pay"});
-            this.loadToolbar();            
+            console.log(this.toolbarComponent.toolbarController.$el);
+            this.loadToolbar();
+            $('#toolbar').html(this.toolbarComponent.toolbarController.$el);
+            console.log("entro");
         },
         
         
@@ -54,39 +66,14 @@ define(['component/listComponent', 'component/toolbarComponent' ], function(list
                 show: true
             });
 
-            this.purchaseIntegrator.loadButtonsByName([{
+            this.toolbarComponent.addButton({
                 name: 'pay',
+                icon: '',
                 displayName: 'Pay',
-                callback: this.purchaseIntegrator.pay,
-                toolbar: this.toolbarComponent,
-                that: this
-            }]);
-        
-            this.purchaseIntegrator.loadButtonsByName([{
-                name: 'cancel',
-                displayName: 'Cancel',
-                callback: this.purchaseIntegrator.cancel,
-                toolbar: this.toolbarComponent,
-                that: this
-            }]);
-        
+                show: true
+            }, this.purchaseIntegrator.pay, this);
             this.toolbarComponent.render();
-            $('#toolbar').html(this.toolbarComponent.toolbarController.$el);            
-        },
-        
-        productList: function(){
-            this.listComponent = new listCp({componentId: "list", name: "Products"});
-            this.listComponent.initialize({componentId: "list", name: "Products"});
-            this.listComponent.addColumn('productName', 'Product Name');
-            this.listComponent.addColumn('unitValue', 'Unit Value');
-            this.listComponent.addColumn('amount', 'Amount');
-            this.listComponent.addColumn('totalValue', 'Total Value', null);
-            this.listComponent.render();
-            $('#list').html(this.listComponent.listController.$el);
-        },
-        
-        render: function(parent){
-            $('#'+parent).append($('#bill').show());
+            console.log(this.toolbarComponent.el);
         }
     });
     
--- Marketplace/purchase.web/src/main/webapp/src/component/purchaseIntegrator.js
+++ Marketplace/purchase.web/src/main/webapp/src/component/purchaseIntegrator.js
@@ -18,9 +18,9 @@ define(['component/addressComponent', 'component/creditCardComponent', 'componen
             this.addressComponent.clearGlobalActions();
             this.addressComponent.clearRecordActions();
             this.addressComponent.addGlobalAction({
-                name: 'cancel',
+                name: 'cancelar',
                 icon: '',
-                displayName: 'Cancel',
+                displayName: 'Cancelar',
                 show: true
             }, this.cancel, this);
             this.addressComponent.addRecordAction({
@@ -56,9 +56,9 @@ define(['component/addressComponent', 'component/creditCardComponent', 'componen
             this.creditCardComponent.clearGlobalActions();
             this.creditCardComponent.clearRecordActions();            
             this.creditCardComponent.addGlobalAction({
-                name: 'cancel',
+                name: 'cancelar',
                 icon: '',
-                displayName: 'Cancel',
+                displayName: 'Cancelar',
                 show: true
             }, this.cancel, this);
             this.creditCardComponent.addRecordAction({
@@ -94,7 +94,7 @@ define(['component/addressComponent', 'component/creditCardComponent', 'componen
                         
             $('#main').html('');
             this.billComponent = new billCp();
-            this.billComponent.initialize({addressList:this.selectedAddress, creditCardList:this.selectedPayment, purchaseIntegrator:this});
+            this.billComponent.initialize({addressList:this.selectedAddress, paymentList:this.selectedPayment, purchaseIntegrator:this});
             $('.breadcrumb').html('');
             this.billComponent.render('main');
             $('.breadcrumb').append('<li>Shopping Address</li>');
@@ -177,19 +177,12 @@ define(['component/addressComponent', 'component/creditCardComponent', 'componen
            
         },
         
-        cancel: function(){
-            document.location.href="http://localhost:8084/purchase.web/";
+        useBonus: function(){
+            
         },
         
-        loadButtonsByName: function(param){
-            $.each(param, function(key, value){
-                value.toolbar.addButton({
-                    name: value.name,
-                    icon: '',
-                    displayName: value.displayName,
-                    show: true
-                }, value.callback, value.that);
-            });
+        cancel: function(){
+            document.location.href="http://localhost:8080/purchase.web";
         }
     });
     return App.Component.PurchaseIntegrator;
