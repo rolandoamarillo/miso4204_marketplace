3ddb7fdd1da81ef6d7e2c3403d1352e5fec1f205
Jhonatan <jhonatan.alarcon.ariza@gmail.com>
2014-11-25 03:02:14 -0500

    Actualizacion de Login
    Se tiene en cuenta el logout de google+

--- Marketplace/user.web/src/main/webapp/src/component/logout.js
+++ Marketplace/user.web/src/main/webapp/src/component/logout.js
@@ -3,50 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//function getCookie(cname) {
-//    var name = cname + "=";
-//    var ca = document.cookie.split(';');
-//    for (var i = 0; i < ca.length; i++) {
-//        var c = ca[i];
-//        while (c.charAt(0) == ' ')
-//            c = c.substring(1);
-//        if (c.indexOf(name) != -1)
-//            return c.substring(name.length, c.length);
-//    }
-//    return "";
-//}
-//
-//function delete_cookie(name, value) {
-//    document.cookie = name + '='+ value+'; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/"; ';
-//}
+
+
 function setCookie(cname, cvalue, exdays) {
-            var d = new Date();
-            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-            var expires = "expires=" + d.toUTCString();
-            var path = "; path=/";
-            document.cookie = cname + "=" + cvalue + "; " + expires + path;
-        }
-
-
-
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    var path = "; path=/";
+    document.cookie = cname + "=" + cvalue + "; " + expires + path;
+}
 
 var full = location.hostname + (location.port ? ':' + location.port : '');
-//var cookie_name = 'token';
-//var cookie_token = getCookie("token");
-//delete_cookie(cookie_name, cookie_token);
 setCookie("token", "nodata", -1);
 $.ajax({
     type: "POST",
     url: "http://" + full + "/user.services/webresources/auth/logout",
-//          crossDomain: true,
+//  crossDomain: true,
     data: {},
     dataType: 'json',
     contentType: "application/json"
 }).done(_.bind(function (data) {
     console.log(data);
+    win = window.open("https://mail.google.com/mail/u/0/?logout&hl=en", "something", "width=550,height=570");
+    setTimeout("win.close();", 4000);
 }, this)).error(_.bind(function (data) {
     console.log("data");
     alert('Logout : ' + data["responseText"]);
 
 }, this));
-
